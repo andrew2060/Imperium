3bbda31d31f7f5d960ed06af9780a9bea8dc0d75
Andrew2060 <2060andrew@gmail.com>
2015-03-30 11:22:07 -0700

    Remove duplicate code

--- src/main/java/net/kingdomsofarden/townships/listeners/BlockProtectionListener.java
+++ src/main/java/net/kingdomsofarden/townships/listeners/BlockProtectionListener.java
@@ -31,6 +31,20 @@ public class BlockProtectionListener implements Listener {
     }
 
 
+    private boolean hasAccess(Collection<Region> boundingRegions, Citizen c, AccessType access) {
+        HashSet<RoleGroup> effectiveRoles = new HashSet<RoleGroup>();
+        for (Region region : boundingRegions) {
+            effectiveRoles.addAll(region.getRoles(c));
+            if (region.hasEffect("protection")) {
+                EffectProtection effect = region.getEffect("protection");
+                if (effect.isPermitted(AccessType.ACCESS, c, effectiveRoles)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
@@ -38,34 +52,18 @@ public class BlockProtectionListener implements Listener {
             if (Constants.ACCESS_TYPES.contains(b.getType())) {
                 Collection<Region> boundingRegions = plugin.getRegions().getBoundingRegions(event.getClickedBlock().getLocation());
                 Citizen c = Townships.getCitizens().getCitizen(event.getPlayer().getUniqueId());
-                HashSet<RoleGroup> effectiveRoles = new HashSet<RoleGroup>();
-                for (Region region : boundingRegions) {
-                    effectiveRoles.addAll(region.getRoles(c));
-                    if (region.hasEffect("protection")) {
-                        EffectProtection effect = region.getEffect("protection");
-                        if (effect.isPermitted(AccessType.ACCESS, c, effectiveRoles)) {
-                            return;
-                        }
-                    }
+                if (!hasAccess(boundingRegions, c, AccessType.ACCESS)) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_ACCESS);
                 }
-                event.setCancelled(true);
-                event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_ACCESS);
             } else if (Constants.INTERACT_TYPES.contains(b.getType())
                     || (Constants.PROTECT_FIRE && event.getItem() != null && event.getItem().getType().equals(Material.FLINT_AND_STEEL))) {
                 Collection<Region> boundingRegions = plugin.getRegions().getBoundingRegions(event.getClickedBlock().getLocation());
                 Citizen c = Townships.getCitizens().getCitizen(event.getPlayer().getUniqueId());
-                HashSet<RoleGroup> effectiveRoles = new HashSet<RoleGroup>();
-                for (Region region : boundingRegions) {
-                    effectiveRoles.addAll(region.getRoles(c));
-                    if (region.hasEffect("protection")) {
-                        EffectProtection effect = region.getEffect("protection");
-                        if (effect.isPermitted(AccessType.INTERACT, c, effectiveRoles)) {
-                            return;
-                        }
-                    }
+                if (!hasAccess(boundingRegions, c, AccessType.INTERACT)) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_INTERACT);
                 }
-                event.setCancelled(true);
-                event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_INTERACT);
             }
         }
     }
@@ -74,35 +72,19 @@ public class BlockProtectionListener implements Listener {
     public void onBlockPlace(BlockPlaceEvent event) {
         Collection<Region> boundingRegions = plugin.getRegions().getBoundingRegions(event.getBlock().getLocation());
         Citizen c = Townships.getCitizens().getCitizen(event.getPlayer().getUniqueId());
-        HashSet<RoleGroup> effectiveRoles = new HashSet<RoleGroup>();
-        for (Region region : boundingRegions) {
-            effectiveRoles.addAll(region.getRoles(c));
-            if (region.hasEffect("protection")) {
-                EffectProtection effect = region.getEffect("protection");
-                if (effect.isPermitted(AccessType.ACCESS, c, effectiveRoles)) {
-                    return;
-                }
-            }
+        if (!hasAccess(boundingRegions, c, AccessType.CONSTRUCT)) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_CONSTRUCT);
         }
-        event.setCancelled(true);
-        event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_CONSTRUCT);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
         Collection<Region> boundingRegions = plugin.getRegions().getBoundingRegions(event.getBlock().getLocation());
         Citizen c = Townships.getCitizens().getCitizen(event.getPlayer().getUniqueId());
-        HashSet<RoleGroup> effectiveRoles = new HashSet<RoleGroup>();
-        for (Region region : boundingRegions) {
-            effectiveRoles.addAll(region.getRoles(c));
-            if (region.hasEffect("protection")) {
-                EffectProtection effect = region.getEffect("protection");
-                if (effect.isPermitted(AccessType.ACCESS, c, effectiveRoles)) {
-                    return;
-                }
-            }
+        if (!hasAccess(boundingRegions, c, AccessType.CONSTRUCT)) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_CONSTRUCT);
         }
-        event.setCancelled(true);
-        event.getPlayer().sendMessage(I18N.NO_PERMISSION_AREA_CONSTRUCT);
     }
 }
