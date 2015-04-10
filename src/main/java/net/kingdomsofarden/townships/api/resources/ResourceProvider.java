9cad188cc6ad83d34ee0d7d605beb02994c54382
Andrew2060 <2060andrew@gmail.com>
2015-04-09 21:54:32 -0700

    A physical chest item provider

--- src/main/java/net/kingdomsofarden/townships/api/resources/ResourceProvider.java
+++ src/main/java/net/kingdomsofarden/townships/api/resources/ResourceProvider.java
@@ -2,5 +2,4 @@ package net.kingdomsofarden.townships.api.resources;
 
 public interface ResourceProvider {
     int getPriority();
-    String getName();
 }
---
+++ src/main/java/net/kingdomsofarden/townships/resources/PhysicalChestItemProvider.java
@@ -0,0 +1,76 @@
+package net.kingdomsofarden.townships.resources;
+
+import net.kingdomsofarden.townships.api.resources.ItemProvider;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class PhysicalChestItemProvider implements ItemProvider {
+
+
+    private Location location;
+    private int priority;
+
+    public PhysicalChestItemProvider(Location location, int priority) {
+        this.location = location;
+        this.priority = priority;
+    }
+
+    @Override
+    public int getAmount(Material mat) {
+        Block b = location.getBlock();
+        Chest chest;
+        if (b.getType() == Material.CHEST) {
+            chest = (Chest) b.getState();
+        } else {
+            return 0;
+        }
+        int amt = 0;
+        for (Entry<Integer, ? extends ItemStack> entry : chest.getBlockInventory().all(mat).entrySet()) {
+             amt += entry.getValue().getAmount();
+        }
+        return amt;
+    }
+
+    @Override
+    public int remove(Material mat, int max) {
+        Block b = location.getBlock();
+        Chest chest;
+        if (b.getType() == Material.CHEST) {
+            chest = (Chest) b.getState();
+        } else {
+            return 0;
+        }
+        int amt = max;
+        Map<Integer, Integer> slotRemoveAmts = new HashMap<Integer, Integer>();
+        for (Entry<Integer, ? extends ItemStack> entry : chest.getBlockInventory().all(mat).entrySet()) {
+            int removable = entry.getValue().getAmount();
+            removable = removable > amt ? amt : removable;
+            slotRemoveAmts.put(entry.getKey(), removable);
+            amt -= removable;
+        }
+        Inventory inv = chest.getBlockInventory();
+        for (Entry<Integer, Integer> entry : slotRemoveAmts.entrySet()) {
+            ItemStack i = inv.getItem(entry.getKey());
+            if (i.getAmount() == entry.getValue()) {
+                inv.clear(entry.getKey());
+            } else {
+                i.setAmount(entry.getValue());
+            }
+        }
+        return max - amt;
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+}
