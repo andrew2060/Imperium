793c7b174cfe1d8b93ca5fd1b111708a5bce97c3
Andrew2060 <2060andrew@gmail.com>
2015-04-22 23:35:28 -0700

    Begin work on consistancy check

---
+++ Townships-Plugin/src/main/java/net/kingdomsofarden/townships/listeners/RegionalConsistancyListener.java
@@ -0,0 +1,22 @@
+package net.kingdomsofarden.townships.listeners;
+
+import net.kingdomsofarden.townships.api.Townships;
+import net.kingdomsofarden.townships.api.regions.Region;
+import net.kingdomsofarden.townships.util.MetaKeys;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class RegionalConsistancyListener implements Listener {
+    @EventHandler(priority= EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakEvent event) {
+        Location loc = event.getBlock().getLocation();
+        for (Region r : Townships.getRegions().getBoundingRegions(loc)) {
+            if (r.getMetadata().containsKey(MetaKeys.REQUIREMENT_BLOCK)) {
+
+            }
+        }
+    }
+}
--- Townships-Plugin/src/main/java/net/kingdomsofarden/townships/util/MetaKeys.java
+++ Townships-Plugin/src/main/java/net/kingdomsofarden/townships/util/MetaKeys.java
@@ -10,4 +10,6 @@ public class MetaKeys {
     public static final String POWER_SHIELD_MAX = "POWER_SHIELD_MAX";
     public static final String POWER_SHIELD_CURR = "POWER_SHIELD_CURR";
     public static final String POWER_SHIELD_RESET = "POWER_SHIELD_RESET";
+
+    public static final String REQUIREMENT_BLOCK = "BLOCK_REQUIREMENT";
 }
