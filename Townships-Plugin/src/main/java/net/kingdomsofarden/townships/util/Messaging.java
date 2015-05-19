e22a07e5211f7403689a1a4757f509981eadc686
Andrew Wen <2060andrew@gmail.com>
2015-05-18 18:31:18 -0700

    String replace doesn't change original string

--- Townships-Plugin/src/main/java/net/kingdomsofarden/townships/util/Messaging.java
+++ Townships-Plugin/src/main/java/net/kingdomsofarden/townships/util/Messaging.java
@@ -7,7 +7,7 @@ public class Messaging {
     public static void sendFormattedMessage(CommandSender recipient, String message, Object... args) {
         message = I18N.GLOBAL_PREFIX + message;
         for (int i = 0; i < args.length; i++) {
-            message.replace("$" + i, args[i] + "");
+            message = message.replace("$" + i, args[i] + "");
         }
         recipient.sendMessage(message);
     }
@@ -15,8 +15,15 @@ public class Messaging {
     public static void broadcastFormattedMessage(String message, Object... args) {
         message = I18N.GLOBAL_PREFIX + message;
         for (int i = 0; i < args.length; i++) {
-            message.replace("$" + i, args[i] + "");
+            message = message.replace("$" + i, args[i] + "");
         }
         Bukkit.broadcastMessage(message);
+    }
+
+    public static String format(String message, Object... args) {
+        for (int i = 0; i < args.length; i++) {
+            message = message.replace("$" + i, args[i] + "");
+        }
+        return message;
     }
 }
